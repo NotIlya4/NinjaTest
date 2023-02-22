5e518514342662ed438f9eb51439b4c636e718ea
NotIlya <secretman44444@gmail.com>
2023-02-23 02:24:46 +0300

    Add Math tests

---
+++ NinjaTest.UnitTests/MathTests.cs
@@ -0,0 +1,33 @@
+﻿using Math = NinjaTest.Fundamentals.Math;
+
+namespace NinjaTest.Test;
+
+public class MathTests
+{
+    private Math _math = null!;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        _math = new();
+    }
+    
+    [Test]
+    public void Add_WhenCalled_ReturnSumOfArguments()
+    {
+        int result = _math.Add(1, 2);
+
+        Assert.That(result, Is.EqualTo(3));
+    }
+
+    [Test]
+    [TestCase(2, 1 ,2)]
+    [TestCase(1, 2, 2)]
+    [TestCase(1, 1, 1)]
+    public void Max_WhenCalled_ReturnGraterArgument(int a, int b, int expectedResult)
+    {
+        int result = _math.Max(a, b);
+        
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
