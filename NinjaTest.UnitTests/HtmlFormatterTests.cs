203e81c1445ed1da05845d3df53d698806de0a2c
NotIlya <secretman44444@gmail.com>
2023-02-23 02:33:34 +0300

    Add HtmlFormatter tests

---
+++ NinjaTest.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,22 @@
+﻿using TestNinja.Fundamentals;
+
+namespace NinjaTest.Test;
+
+public class HtmlFormatterTests
+{
+    private HtmlFormatter _htmlFormatter = null!;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        _htmlFormatter = new HtmlFormatter();
+    }
+    
+    [Test]
+    public void FormatAsBold_WhenCalled_EncloseStringWithStrongTag()
+    {
+        string result = _htmlFormatter.FormatAsBold("abc");
+        
+        Assert.That(result, Is.EqualTo("<strong>abc</strong>"));
+    }
+}
