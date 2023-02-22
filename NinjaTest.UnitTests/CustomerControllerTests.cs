04942fb86e519685d23bb281cafed6d0872e80bf
NotIlya <secretman44444@gmail.com>
2023-02-23 02:45:30 +0300

    Add CustomerController tests

---
+++ NinjaTest.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using TestNinja.Fundamentals;
+
+namespace NinjaTest.Test;
+
+public class CustomerControllerTests
+{
+    private CustomerController _customerController = null;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _customerController = new();
+    }
+    
+    [Test]
+    public void GetCustomer_IdIsZero_ReturnNotFound()
+    {
+        var result = _customerController.GetCustomer(0);
+        
+        Assert.That(result, Is.TypeOf<NotFound>());
+    }
+
+    [Test]
+    public void GetCustomer_IdIsNotZero_ReturnOk()
+    {
+        var result = _customerController.GetCustomer(1);
+        
+        Assert.That(result, Is.TypeOf<Ok>());
+    }
+}
