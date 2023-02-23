830e24894a3c72fa638b7d81f3db613e55ee63bb
NotIlya <secretman44444@gmail.com>
2023-02-23 22:47:05 +0300

    Add InstallerHelper tests

---
+++ NinjaTest.UnitTests/Mocking/InstallerHelperTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using Moq;
+using NinjaTest.Mocking;
+
+namespace NinjaTest.Test.Mocking;
+
+public class InstallerHelperTests
+{
+    private Mock<IFileDownloader> _fileDownloader = null!;
+    private InstallerHelper _installerHelper = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _fileDownloader = new Mock<IFileDownloader>();
+        _installerHelper = new InstallerHelper(_fileDownloader.Object);
+    }
+
+    [Test]
+    public void DownloadInstaller_DownloadFails_ReturnFalse()
+    {
+        _fileDownloader.Setup(
+            fd => fd.DownloadFile(It.IsAny<string>(), It.IsAny<string>()))
+            .Throws<WebException>();
+
+        var result = _installerHelper.DownloadInstaller("customer", "installer");
+        
+        Assert.False(result);
+    }
+    
+    [Test]
+    public void DownloadInstaller_DownloadCompletes_ReturnTrue()
+    {
+        var result = _installerHelper.DownloadInstaller("customer", "installer");
+        
+        Assert.True(result);
+    }
+}
